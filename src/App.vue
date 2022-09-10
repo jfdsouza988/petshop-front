57f0906247d38f5cb7383ab87bfad46aab23ed6d
Julio Souza <julio3g@pop-os.localdomain>
2022-09-10 14:19:35 -0300

    feat: add app structure

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <Sidebar />
+    </div>
+    <div class="column is-three-quarter content">
+      <Notifications />
+      <router-view />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import Sidebar from './components/Sidebar.vue';
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Sidebar
   }
 });
 </script>
@@ -22,6 +28,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
