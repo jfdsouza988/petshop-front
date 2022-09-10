8bab0762a3687ca4ab8d4a3c29c3cac0d0dd70cf
Julio Souza <julio3g@pop-os.localdomain>
2022-09-10 14:20:03 -0300

    feat: add router and store in main file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@fortawesome/fontawesome-free/css/all.css'
+import router from './router'
+import { key, store } from './store'
+
+createApp(App).use(router).use(store, key).mount('#app')
